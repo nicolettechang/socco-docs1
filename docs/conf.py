7ca0408bb86dfd63584d38415f65c413d2912e96
nicolettechang <59173035+nicolettechang@users.noreply.github.com>
2025-07-02 14:05:26 +0200

    Initial commit with Sphinx documentation

---
+++ docs/conf.py
@@ -0,0 +1,34 @@
+# docs/conf.py
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'socco-docs1'
+copyright = '2025, Nicolette Chang'
+author = 'Nicolette Chang'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
+    'numpydoc',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,15 @@
+.. socco-docs1 documentation master file, created by
+   sphinx-quickstart on Wed Jul  2 13:36:45 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to socco-docs1's documentation!
+=======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   guides/index
+   models/index
+
